fe19b9cb0c85b6ffa654bb47d731e76620c3e027
Tran Minh Hai <hai@bio-rithm.com>
2021-12-12 13:56:01 +0700

    navbar and container

--- pages/navbar.tsx
+++ pages/navbar.tsx
@@ -1,5 +1,5 @@
 import { ReactNode   } from 'react';
-import { Avatar, Box, Flex, HStack, IconButton, Link, Menu, MenuButton, Stack, useDisclosure, Text} from '@chakra-ui/react';
+import { Avatar, Box, Flex, HStack, IconButton, Link, Menu, MenuButton, Stack, useDisclosure, Text, Container} from '@chakra-ui/react';
 import { HamburgerIcon, CloseIcon} from '@chakra-ui/icons';
 
 
@@ -20,64 +20,66 @@ const NavLink = ( { children } : { children: ReactNode }) => {
 const Navbar = () => {
   const {isOpen, onOpen, onClose} = useDisclosure();  
   return (
-    <>
-      <Box bg='gray.200' width='100%' px={4}>
-        <Flex alignItems='center' h={16} justifyContent='space-between'>
-          <IconButton
-            display={{ md: 'none' }}
-            size={'md'}
-            icon={ isOpen ? <CloseIcon></CloseIcon> : <HamburgerIcon></HamburgerIcon>}
-            aria-label='Open Menu'
-            onClick={isOpen ? onClose : onOpen}
-          >
-          </IconButton>
-          <HStack spacing={8} alignItems='center'>
-            <Box>Logo</Box>
-            <HStack 
-              spacing={4} 
-              as='nav' 
-              display={{ base: 'none', md: 'flex' }}
+    <Box width='100%' bg='gray.100'>
+      <Container maxWidth={'container.lg'}>
+        <Box width='100%' px={4}>
+          <Flex alignItems='center' h={16} justifyContent='space-between'>
+            <IconButton
+              display={{ md: 'none' }}
+              size={'md'}
+              icon={ isOpen ? <CloseIcon></CloseIcon> : <HamburgerIcon></HamburgerIcon>}
+              aria-label='Open Menu'
+              onClick={isOpen ? onClose : onOpen}
             >
-              {['Dashboard','Projects','Team'].map((name,index) => {
-                return (
-                  <NavLink key={index}>{name}</NavLink> 
-                );
-              })}
+            </IconButton>
+            <HStack spacing={8} alignItems='center'>
+              <Box>Logo</Box>
+              <HStack 
+                spacing={4} 
+                as='nav' 
+                display={{ base: 'none', md: 'flex' }}
+              >
+                {['Dashboard','Projects','Team'].map((name,index) => {
+                  return (
+                    <NavLink key={index}>{name}</NavLink> 
+                  );
+                })}
+              </HStack>
             </HStack>
-          </HStack>
-          <Flex>
-            <Menu>
-              <MenuButton>
-                <Avatar size={'sm'}></Avatar>
-              </MenuButton>
-            </Menu>
+            <Flex>
+              <Menu>
+                <MenuButton>
+                  <Avatar size={'sm'}></Avatar>
+                </MenuButton>
+              </Menu>
+            </Flex>
           </Flex>
-        </Flex>
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack>
-              {['Dashboard','Projects','Team'].map((name,index)=>{
-                return (
-                  <Flex 
-                    py={2}
-                    as={Link}
-                    href={'#'}
-                    justify={'space-between'}
-                    align={'center'}
-                    _hover={{textDecoration:'none'}}
-                    key={index}
-                  >
-                    <Text fontWeight={600} color={'gray.600'}>
-                      {name}
-                    </Text>
-                  </Flex>
-                )
-              })}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </>
+          {isOpen ? (
+            <Box pb={4} display={{ md: 'none' }}>
+              <Stack>
+                {['Dashboard','Projects','Team'].map((name,index)=>{
+                  return (
+                    <Flex 
+                      py={2}
+                      as={Link}
+                      href={'#'}
+                      justify={'space-between'}
+                      align={'center'}
+                      _hover={{textDecoration:'none'}}
+                      key={index}
+                    >
+                      <Text fontWeight={600} color={'gray.600'}>
+                        {name}
+                      </Text>
+                    </Flex>
+                  )
+                })}
+              </Stack>
+            </Box>
+          ) : null}
+        </Box>
+      </Container>
+    </Box>
   );
 }
 
