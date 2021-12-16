09518ee3798afdffaa0a55df2296e5717117012c
Tran Minh Hai <hai@bio-rithm.com>
2021-12-16 11:20:34 +0700

    after and before pseudo with box

--- pages/box/test.tsx
+++ pages/box/test.tsx
@@ -4,7 +4,7 @@
 // Box display flex alignItems-center, justifyContent-center 
 // Box display flex alignItems-center, justifyContent-space-between
 
-import { Box, Button, Text } from '@chakra-ui/react'; 
+import { Avatar, Badge, Box, Button, Heading, HStack, Stack, Text } from '@chakra-ui/react'; 
 
 const BasicBox = () => {
   return (
@@ -75,6 +75,123 @@ const BoxTextAlign = () => {
   ); 
 }
 
+const BoxCard = () => {
+  return (
+    <Box
+      bg='gray.200'
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      width='100%'
+    >
+      <Box
+        textAlign='center'
+        maxW={'320px'}
+        rounded='lg'
+        w='full'
+        p={6}
+        m={6}
+        bg='white'
+        boxShadow={'2xl'}
+      >
+        <Avatar
+          size='xl'
+          alt='Avatar Alt'
+          mb={4}
+          pos={'relative'}
+          _after={{
+            content: '""',
+            w: 6,
+            h: 6,
+            bg: 'green.300',
+            border: '2px solid white',
+            rounded: 'full',
+            pos: 'absolute',
+            bottom: '0px', 
+            right: '0px'
+          }}
+          icon={<Text>HT</Text>}
+        >
+        </Avatar>
+        <Heading
+          fontSize='2xl' 
+          fontFamily='body'
+        >
+          Hai Tran
+        </Heading>
+        <Text
+          fontWeight={600}
+          color='gray.500'
+          mb={4}
+        >
+          @lindsey_jam3s
+        </Text>
+        <Text
+          textAlign='center'
+          color='gray.700'
+          px={3}
+        >
+          Actress, musician, songwriter, and artist. PM for work inquries
+        </Text>
+        <Stack align='center' justify='center' direction='row' mt={6}>
+          <Badge
+            px={2}
+            py={2}
+            bg={'gray.50'}
+          >
+            #art 
+          </Badge>
+          <Badge
+            px={2}
+            py={2}
+            bg={'gray.50'}
+          >
+            #music 
+          </Badge>
+          <Badge
+            px={2}
+            py={2}
+            bg={'gray.50'}
+          >
+            #photography 
+          </Badge>
+        </Stack>
+        <HStack
+          justifyContent='space-between'
+          mt={8}
+          spacing={4}
+        >
+          <Button
+            flex={1}
+            fontSize='sm'
+            rounded='full'
+            _focus={{
+              bg:'gray.200'
+            }}
+          >
+            Message
+          </Button>
+          <Button 
+            flex={1}
+            fontSize='sm'
+            rounded='full'
+            bg={'blue.400'}
+            color='white'
+            _hover={{
+              bg:'blue.500'
+            }}
+            _focus={{
+              bg:'blue.500'
+            }}
+          >
+            Follow
+          </Button>
+        </HStack>
+      </Box>
+    </Box>
+  ); 
+}
+
 const BasicLayout = () => {
   return (
     <Box width='100%'>
@@ -86,6 +203,8 @@ const BasicLayout = () => {
       <FlexBoxSpaceBetween></FlexBoxSpaceBetween>
       <Text>Box display-flex, flexDirection-column, alignItems-center, justifyContent-center</Text>
       <BoxTextAlign></BoxTextAlign>
+      <Text>Creat a Login-Card-Profile using Box</Text>
+      <BoxCard></BoxCard>
     </Box>
   ); 
 }
