3e1a38c380b569878ca35382e9c14cba1ad63a2b
Tran Minh Hai <hai@bio-rithm.com>
2021-12-16 14:10:55 +0700

    add three price cards

--- pages/box/test.tsx
+++ pages/box/test.tsx
@@ -194,6 +194,89 @@ const BoxCard = () => {
   ); 
 }
 
+const BoxPriceCard = () => {
+  return (
+    <Box
+      borderWidth='1px'
+      borderRadius={'xl'}
+      borderColor='gray.200'
+      shadow='base'
+      rounded='lg'
+      bg='white'
+      display='flex'
+      flexDirection='column'
+      alignItems='center'
+      justifyContent='center'
+      position='relative'
+    >
+      <Box
+        position="absolute"
+        top="-16px"
+        left="50%"
+        style={{ transform: 'translate(-50%)' }}>
+        <Text
+          textTransform="uppercase"
+          bg={useColorModeValue('red.300', 'red.700')}
+          px={3}
+          py={1}
+          color={useColorModeValue('gray.900', 'gray.300')}
+          fontSize="sm"
+          fontWeight="600"
+          rounded="xl">
+                Most Popular
+        </Text>
+      </Box>
+      <Box py={4} px={12} textAlign='center'>
+        <Text fontWeight="500" fontSize="2xl">
+                Growth
+        </Text>
+        <HStack justifyContent="center">
+          <Text fontSize="3xl" fontWeight="600">
+                  $
+          </Text>
+          <Text fontSize="5xl" fontWeight="900">
+                  149
+          </Text>
+          <Text fontSize="3xl" color="gray.500">
+                  /month
+          </Text>
+        </HStack>
+      </Box>
+      <VStack
+        bg={useColorModeValue('gray.50', 'gray.700')}
+        py={4}
+        borderBottomRadius={'xl'}>
+        <List spacing={3} textAlign="start" px={12}>
+          <ListItem>
+            <ListIcon as={FaCheckCircle} color="green.500" />
+                  unlimited build minutes
+          </ListItem>
+          <ListItem>
+            <ListIcon as={FaCheckCircle} color="green.500" />
+                  Lorem, ipsum dolor.
+          </ListItem>
+          <ListItem>
+            <ListIcon as={FaCheckCircle} color="green.500" />
+                  5TB Lorem, ipsum dolor.
+          </ListItem>
+          <ListItem>
+            <ListIcon as={FaCheckCircle} color="green.500" />
+                  5TB Lorem, ipsum dolor.
+          </ListItem>
+          <ListItem>
+            <ListIcon as={FaCheckCircle} color="green.500" />
+                  5TB Lorem, ipsum dolor.
+          </ListItem>
+        </List>
+        <Box w="80%" pt={7}>
+          <Button w="full" colorScheme="red">
+                  Start trial
+          </Button>
+        </Box>
+      </VStack>
+    </Box>
+  ); 
+}
 
 const BoxPrice = () => {
   return (
@@ -205,89 +288,47 @@ const BoxPrice = () => {
       height='700px' 
       position='relative'
     >
-      <Box
-        borderWidth='1px'
-        borderRadius={'xl'}
-        borderColor='gray.200'
-        shadow='base'
-        rounded='lg'
-        bg='white'
-        display='flex'
-        flexDirection='column'
-        alignItems='center'
-        justifyContent='center'
-        position='relative'
-      >
-        <Box
-          position="absolute"
-          top="-16px"
-          left="50%"
-          style={{ transform: 'translate(-50%)' }}>
-          <Text
-            textTransform="uppercase"
-            bg={useColorModeValue('red.300', 'red.700')}
-            px={3}
-            py={1}
-            color={useColorModeValue('gray.900', 'gray.300')}
-            fontSize="sm"
-            fontWeight="600"
-            rounded="xl">
-                Most Popular
-          </Text>
-        </Box>
-        <Box py={4} px={12} textAlign='center'>
-          <Text fontWeight="500" fontSize="2xl">
-                Growth
-          </Text>
-          <HStack justifyContent="center">
-            <Text fontSize="3xl" fontWeight="600">
-                  $
-            </Text>
-            <Text fontSize="5xl" fontWeight="900">
-                  149
-            </Text>
-            <Text fontSize="3xl" color="gray.500">
-                  /month
-            </Text>
-          </HStack>
-        </Box>
-        <VStack
-          bg={useColorModeValue('gray.50', 'gray.700')}
-          py={4}
-          borderBottomRadius={'xl'}>
-          <List spacing={3} textAlign="start" px={12}>
-            <ListItem>
-              <ListIcon as={FaCheckCircle} color="green.500" />
-                  unlimited build minutes
-            </ListItem>
-            <ListItem>
-              <ListIcon as={FaCheckCircle} color="green.500" />
-                  Lorem, ipsum dolor.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={FaCheckCircle} color="green.500" />
-                  5TB Lorem, ipsum dolor.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={FaCheckCircle} color="green.500" />
-                  5TB Lorem, ipsum dolor.
-            </ListItem>
-            <ListItem>
-              <ListIcon as={FaCheckCircle} color="green.500" />
-                  5TB Lorem, ipsum dolor.
-            </ListItem>
-          </List>
-          <Box w="80%" pt={7}>
-            <Button w="full" colorScheme="red">
-                  Start trial
-            </Button>
-          </Box>
-        </VStack>
-      </Box>
+      <BoxPriceCard></BoxPriceCard>
     </Box>
   ); 
 }
 
+
+const BoxThreePriceCard = () => {
+  return (
+    <Box
+      bg='gray.200'
+      display='flex'
+      alignItems='center'
+      justifyContent='center'
+      flexDirection='column'
+      py={10}
+    >
+      <VStack spacing={2} textAlign='center'>
+        <Heading
+          as='h1'
+          fontSize='4xl'
+        >
+          Plans that fit your need
+        </Heading>
+        <Text fontSize='lg' color='gray.500'>
+          Start with 14-day free trial. No credit card needed. Cancel at anytime. 
+        </Text>
+      </VStack>
+      <Stack
+        direction={{ base: 'column', md: 'row' }}
+        textAlign='center'
+        justify='center'
+        spacing={{ base: 4, lg: 10 }}
+        py={10}
+      >
+        <BoxPriceCard></BoxPriceCard>
+        <BoxPriceCard></BoxPriceCard>
+        <BoxPriceCard></BoxPriceCard>
+      </Stack>
+    </Box>
+  ); 
+}
 
 
 const BasicLayout = () => {
@@ -305,6 +346,8 @@ const BasicLayout = () => {
       <BoxCard></BoxCard>
       <Text>Price Box</Text>
       <BoxPrice></BoxPrice>
+      <Text>Three price box</Text>
+      <BoxThreePriceCard></BoxThreePriceCard>
     </Box>
   ); 
 }
