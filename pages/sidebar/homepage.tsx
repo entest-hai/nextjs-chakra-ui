d229e9766a51e6defc236e4daafbbbfa1fcc79fc
Tran Minh Hai <hai@bio-rithm.com>
2022-07-23 09:06:20 +0700

    simple layout

--- pages/sidebar/homepage.tsx
+++ pages/sidebar/homepage.tsx
@@ -60,29 +60,36 @@ const Layout = ({ children }) => {
         justifyContent={'center'}
         ml={{ base: '0', md: '60' }}
       >
-        <Flex maxW={'800px'}>{children}</Flex>
+        <Flex maxW={'800px'} direction={'column'}>
+          {children}
+        </Flex>
       </Flex>
     </Flex>
   )
 }
 
 const HomePage = () => {
+  const items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
   return (
     <Box height={'100vh'}>
       <Navbar></Navbar>
       <Layout>
-        <Text>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad, magnam
-          dolores amet tempore maiores modi est earum eveniet ex adipisci
-          praesentium assumenda nobis ut, voluptate numquam aspernatur, debitis
-          quos error? Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Nesciunt, at voluptate praesentium itaque id architecto neque vel
-          officiis distinctio mollitia ducimus, ab iste sit, eos consectetur
-          temporibus soluta sint? At. Lorem ipsum dolor, sit amet consectetur
-          adipisicing elit. Expedita esse, officia inventore ad eos magni est
-          deleniti placeat dignissimos, totam nihil beatae consequatur odio
-          quos, quia nemo laborum nesciunt doloribus.
-        </Text>
+        {items.map(item => (
+          <Text key={item}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam
+            vel minus doloremque qui obcaecati eligendi rerum, tenetur
+            laboriosam, ipsam, perspiciatis officiis animi incidunt. Veniam cum
+            unde ut officia perspiciatis distinctio. Lorem ipsum dolor, sit amet
+            consectetur adipisicing elit. Laboriosam aperiam id repellat,
+            laborum distinctio rem facilis vel quae impedit, eos repellendus
+            dolorem tempora ipsam consectetur delectus doloremque fugit tenetur
+            officiis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Accusamus consectetur harum minima perspiciatis nam omnis asperiores
+            voluptates, nostrum est dignissimos deleniti quisquam tempore vitae
+            reprehenderit reiciendis quibusdam placeat pariatur voluptate?
+          </Text>
+        ))}
       </Layout>
     </Box>
   )
