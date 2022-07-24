6cbb5ad32c138a48db88aac0cd33d39bb31adae4
Tran Minh Hai <hai@bio-rithm.com>
2022-07-24 12:37:10 +0700

    test simple mdx

--- next.config.js
+++ next.config.js
@@ -3,7 +3,11 @@
 const { withContentlayer } = require('next-contentlayer')
 
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    optimizeFonts: true,
+    modern: true
+  }
 }
 
 module.exports = withContentlayer(nextConfig)
